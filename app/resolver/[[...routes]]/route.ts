972069333884b75127fe9e5c70116e57e994344b
moritz <moritz@fluidkey.com>
2024-03-23 13:40:55 +0300

    properly ingest the resolver request

--- app/resolver/[[...routes]]/route.ts
+++ app/resolver/[[...routes]]/route.ts
@@ -1,4 +1,18 @@
-export async function GET() {
+import { isHex } from "viem";
+
+export async function GET(req: Request) {
+
+  const path = req.url.split('/');
+
+  const resolver = path[2];
+  if (resolver !== '0x0eB8b476B2d346537f302E99419b215d191A7EFa') {
+    return Response.json({ error: 'Invalid resolver' });  
+  }
+
+  const data = path[3].replace('.json', '');
+  if (isHex(data) === false) {
+    return Response.json({ error: 'Invalid data' });
+  }
 
   const options = {
     method: 'GET',
@@ -12,7 +26,7 @@ export async function GET() {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
 
-  const data = await response.json();
+  const data2 = await response.json();
 
-  return Response.json({ address: data.result.user.verifiedAddresses.eth_addresses[0] });
+  return Response.json({ address: data2.result.user.verifiedAddresses.eth_addresses[0] });
 }
